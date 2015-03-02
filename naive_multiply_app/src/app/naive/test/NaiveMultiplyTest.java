f91e21e6ed433187d092dc117d14be0dda229cb1
Anthony <lightweightdevelopment@gmail.com>
2015-03-02 20:39:27 +0000

    Initial commit

---
+++ naive_multiply_app/src/app/naive/test/NaiveMultiplyTest.java
@@ -0,0 +1,13 @@
+package app.naive.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NaiveMultiplyTest {
+
+	@Test
+	public void testDummy() {
+		assertEquals(true, true);
+	}
+}
