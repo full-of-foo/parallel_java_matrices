dab54a577633f9680c05da48c0060f029851a1ed
Anthony <lightweightdevelopment@gmail.com>
2015-03-06 22:10:23 +0000

    Actually multiply the product of vectors this time

--- naive_multiply_app/src/app/naive/main/NaiveMatrixMultiplier.java
+++ naive_multiply_app/src/app/naive/main/NaiveMatrixMultiplier.java
@@ -1,6 +1,5 @@
 package app.naive.main;
 
-
 public final class NaiveMatrixMultiplier {
 
 	/**
@@ -10,16 +9,24 @@ public final class NaiveMatrixMultiplier {
 	 *            multiplying Matrix
 	 * @param a2
 	 *            multiplying Matrix
+	 * @throws IllegalArgumentException
 	 * @return a3
 	 */
 	public static double[][] multiply(double[][] a1, double[][] a2) {
-		int rowAmount = a1.length;
-		int colAmount = a1[0].length;
-		double a3[][] = new double[rowAmount][colAmount];
+		int rowAmount1 = a1.length;
+		int rowAmount2 = a2.length;
+		int colAmount1 = a1[0].length;
+		int colAmount2 = a2[0].length;
+		boolean isSquares = rowAmount1 == rowAmount2 && colAmount1 == colAmount2;
+		boolean isValidNonSquares = colAmount1 == rowAmount2;
+		if (!isSquares && !isValidNonSquares) throw new IllegalArgumentException("Matrix dimensions must agree");
 
-		for (int i = 0; i < rowAmount; i++) {
-			for (int j = 0; j < colAmount; j++) {
-				a3[i][j] = a1[i][j] * a2[i][j];
+		double a3[][] = new double[rowAmount1][colAmount1];
+		for (int i = 0; i < rowAmount1; i++) {
+			for (int j = 0; j < colAmount1; j++) {
+				for (int k = 0; k < rowAmount2; k++) {
+					a3[i][j] += (a1[i][k] * a2[k][j]);
+				}
 			}
 		}
 		return a3;
--- naive_multiply_app/src/app/naive/test/NaiveMultiplyTest.java
+++ naive_multiply_app/src/app/naive/test/NaiveMultiplyTest.java
@@ -1,7 +1,7 @@
 package app.naive.test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import matrix.utils.Matrix;
 
 import org.junit.Test;
@@ -11,24 +11,46 @@ import app.naive.main.NaiveMatrixMultiplier;
 public class NaiveMultiplyTest {
 	
 	@Test
-	public void testMultiply() {
-		Matrix m1 = new Matrix(2, 2, 3);
-		Matrix m2 = new Matrix(2, 2, 3);
+	public void testSimpleMultiply() {
+		Matrix m1 = new Matrix(1, 1, 3);
+		Matrix m2 = new Matrix(1, 1, 3);
+		double data[][] = NaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
+		assertThat(data[0][0], is(9.0));
+	}
+
+	@Test
+	public void testBetterMultiply() {
+		Matrix m1 = new Matrix(2, 3, 3);
+		Matrix m2 = new Matrix(2, 3, 3);
 		double data[][] = NaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
 		
-		assertThat(data[0][0], is(9.0));
-		assertThat(data[0][1], is(9.0));
-		assertThat(data[1][0], is(9.0));
-		assertThat(data[1][1], is(9.0));
+		assertThat(data[0][0], is(18.0));
+		assertThat(data[0][1], is(18.0));
+		assertThat(data[1][0], is(18.0));
+		assertThat(data[1][1], is(18.0));
 	}
 	
 	@Test
-	public void testMultiplyTime() {
-		Matrix m1 = new Matrix(100000, 100, 1);
-		Matrix m2 = new Matrix(100000, 100, 3);
-		double data[][] = NaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
-		
-		assertThat(data[0][0], is(3.0));
-		assertThat(data[0][1], is(3.0));
+	public void testGoodDemensions() {
+		Matrix m1 = new Matrix(4, 4, 2);
+		Matrix m2 = new Matrix(4, 8, 3);
+		assertNotNull(NaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray()));
+		m1 = new Matrix(8, 4, 2);
+		m2 = new Matrix(4, 4, 3);
+		assertNotNull(NaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray()));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testBadDemensions1() {
+		Matrix m1 = new Matrix(4, 4, 2);
+		Matrix m2 = new Matrix(8, 4, 3); // bad row
+		NaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testBadDemensions2() {
+		Matrix m1 = new Matrix(4, 8, 2); // bad col
+		Matrix m2 = new Matrix(4, 4, 3);
+		NaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
 	}
 }
