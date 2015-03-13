8d67ee444d5d49004022281f8477b9fcf9728819
Anthony <lightweightdevelopment@gmail.com>
2015-03-13 15:57:55 +0000

    Added concurrent multiply tests

--- naive_multiply_app/src/app/naive/test/ConcurrentNaiveMatrixMultiplyTest.java
+++ naive_multiply_app/src/app/naive/test/ConcurrentNaiveMatrixMultiplyTest.java
@@ -7,16 +7,15 @@ import matrix.utils.Matrix;
 
 import org.junit.Test;
 
-import app.naive.main.SequentialNaiveMatrixMultiplier;
+import app.naive.main.ConcurrentNaiveMatrixMultiplier;
 
-public class SequentialNaiveMatrixMultiplyTest {
-
+public class ConcurrentNaiveMatrixMultiplyTest {
+	
 	@Test
 	public void testSimpleMultiply() {
 		Matrix m1 = new Matrix(1, 1, 3);
 		Matrix m2 = new Matrix(1, 1, 3);
-		int data[][] = SequentialNaiveMatrixMultiplier.multiply(m1.getArray(),
-				m2.getArray());
+		int data[][] = ConcurrentNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
 		assertThat(data[0][0], is(9));
 	}
 
@@ -24,48 +23,44 @@ public class SequentialNaiveMatrixMultiplyTest {
 	public void testBetterMultiply() {
 		Matrix m1 = new Matrix(2, 3, 3);
 		Matrix m2 = new Matrix(2, 3, 3);
-		int data[][] = SequentialNaiveMatrixMultiplier.multiply(m1.getArray(),
-				m2.getArray());
-
+		int data[][] = ConcurrentNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
+		
 		assertThat(data[0][0], is(18));
 		assertThat(data[0][1], is(18));
 		assertThat(data[1][0], is(18));
 		assertThat(data[1][1], is(18));
 	}
-
+	
 	@Test
 	public void testGoodDemensions() {
 		Matrix m1 = new Matrix(4, 4, 2);
 		Matrix m2 = new Matrix(4, 8, 3);
-		assertNotNull(SequentialNaiveMatrixMultiplier.multiply(m1.getArray(),
-				m2.getArray()));
+		assertNotNull(ConcurrentNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray()));
 		m1 = new Matrix(8, 4, 2);
 		m2 = new Matrix(4, 4, 3);
-		assertNotNull(SequentialNaiveMatrixMultiplier.multiply(m1.getArray(),
-				m2.getArray()));
+		assertNotNull(ConcurrentNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray()));
 	}
-
-	@Test(expected = IllegalArgumentException.class)
+	
+	@Test(expected=IllegalArgumentException.class)
 	public void testBadDemensions1() {
 		Matrix m1 = new Matrix(4, 4, 2);
 		Matrix m2 = new Matrix(8, 4, 3); // bad row
-		SequentialNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
+		ConcurrentNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
 	}
-
-	@Test(expected = IllegalArgumentException.class)
+	
+	@Test(expected=IllegalArgumentException.class)
 	public void testBadDemensions2() {
 		Matrix m1 = new Matrix(4, 8, 2); // bad col
 		Matrix m2 = new Matrix(4, 4, 3);
-		SequentialNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
+		ConcurrentNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray());
 	}
 
-	// ~3.7 seconds
+	
+	// ~2.1 seconds
 	@Test
 	public void testTimes() {
-		Matrix m1 = Matrix.random(800, 800);
+	 	Matrix m1 = Matrix.random(800, 800);
 		Matrix m2 = Matrix.random(800, 800);
-		assertNotNull(SequentialNaiveMatrixMultiplier.multiply(m1.getArray(),
-				m2.getArray()));
-	}
-
+		assertNotNull(ConcurrentNaiveMatrixMultiplier.multiply(m1.getArray(), m2.getArray()));
+	}	
 }
